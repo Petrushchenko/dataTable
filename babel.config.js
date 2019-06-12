93338eb8b523cb38de28758d12145c2fc7ae846e
Petrushchenko <petrushchenko.l@ukr.net>
2019-06-12 14:49:37 +0300

    fix

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,5 @@ module.exports = {
 	presets: [
 		'@vue/app'
 	],
-	module: {
-	rules: [
-		{
-		test: /\.scss$/,
-		use: [
-		  'vue-style-loader',
-		  'css-loader',
-		  'sass-loader'
-		]
-		}
-	]
-	},
+	
 }
